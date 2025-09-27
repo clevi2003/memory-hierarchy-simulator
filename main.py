3dbb179dce56324b688d9f3b74c141093941271d
Claudia Levi <levi.cl@northeastern.edu>
2025-09-27 14:09:36 -0400

    more test traces

--- main.py
+++ main.py
@@ -4,13 +4,8 @@ from mem_hierarchy import MemoryHierarchySimulator
 def main():
     mem_sim_config = Config.from_config_file("tests/trace.config")
     print(mem_sim_config)
-
-# ppn is wrong when dtlb hit
-
-
     simulator = MemoryHierarchySimulator(mem_sim_config)
-    # simulator.simulate("trace_copy.dat")
-    simulator.simulate("tests/long_trace.dat")
+    simulator.simulate("tests/medium_trace.dat")
 
 
 if __name__ == '__main__':
